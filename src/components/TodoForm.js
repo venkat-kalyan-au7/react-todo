bc97f5650417571408d65a3a3339dfcfcf06df61
Venkat Kalyan <60262281+venkat-kalyan-au7@users.noreply.github.com>
2020-08-14 20:18:25 +0530

    FINISHED

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,12 +1,38 @@
-import React from "react"
+import React, { Component } from "react";
 
-
-class InputForm extends React.Component{
-    render(){
-        return(<div>Todo Form</div>)
-    }
+export default class TodoInput extends Component {
+  render() {
+    const { item, handleChange, handleSubmit, editItem } = this.props;
+    return (
+      <div className="card card-body my-3">
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div className="input-group-text bg-primary text-white">
+                <i className="fas fa-book" />
+              </div>
+            </div>
+            <input
+              type="text"
+              className="form-control text-capitalize"
+              placeholder="ADD TODO HERE"
+              value={item}
+              onChange={handleChange}
+            />
+          </div>
+          <button
+            type="submit"
+            disabled={item ? false : true}
+            className={
+              editItem
+                ? "btn btn-block btn-success mt-3"
+                : "btn btn-block btn-primary mt-3 text-uppercase"
+            }
+          >
+            {editItem ? "EDIT TODO" : "ADD NEW TODO"}
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
-
-
-
-export default InputForm
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,29 @@
-import React from "react"
-import Item from "./TodoItem"
-
-class TodoList extends React.Component{
-    render(){
-      return( <div>
-                todo list
-                <Item/>
-              </div>)
-    }
+import React, { Component } from "react";
+import TodoItem from "./TodoItem";
+export default class TodoList extends Component {
+  render() {
+    const { items, clearList, handleDelete, handleEdit } = this.props;
+    return (
+      <ul className="list-group my-5">
+        <h3 className="text-capitalize text-center">YOUR TODO LIST</h3>
+        {items.map(item => {
+          return (
+            <TodoItem
+              key={item.id}
+              title={item.title}
+              handleDelete={() => handleDelete(item.id)}
+              handleEdit={() => handleEdit(item.id)}
+            />
+          );
+        })}
+        <button
+          type="button"
+          className="btn btn-danger btn-block text-uppercase mt-5"
+          onClick={clearList}
+        >
+          clear list
+        </button>
+      </ul>
+    );
   }
-
-export default TodoList
+}
